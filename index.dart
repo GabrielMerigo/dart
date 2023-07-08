196ddf8526792e8c6009c7f296974c9d63b60f10
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2023-07-08 15:49:18 -0300

    mrore acs

--- index.dart
+++ index.dart
@@ -1,72 +1,14 @@
-// void main() {
-//   String name = "Gabriel";
-//   String sobrenome = "Junior";
+class Animal {
+  String tipo;
+  String cor;
+  int idade;
 
-//   print(name + sobrenome);
-// }
-
-// void main() {
-//   int valor = 10;
-//   double valorQuebrado = 12.2;
-
-//   print(valor);
-//   print(valorQuebrado);
-// }
-
-// void main() {
-//   bool status = true;
-//   bool isTrue = 1 > 0;
-//   String resultado = status ? "Verdadeiro" : "Falso";
-
-//   print(resultado);
-// }
-
-// var nome = "Izzac";
-// var numero = 12;
-
-// dynamic sobrenome = "Pedro"
-// dynamic valor = 12.4
-
-// void main(){
-//   double num1 = 5.5;
-//   doub
-// }
-
-// void main() {
-//   List list = ['Izac', "Gabriel", "123"];
-
-//   list.add("Preto");
-//   list.insert(1, "123");
-
-//   print(list);
-// }
-
-// void main() {
-//   List list = ['Izac', "Gabriel", "123"];
-
-//   list.add("Preto");
-//   list.insert(1, "123");
-
-//   print(list);
-// }
-
-// void main() {
-//   var usuario = {1: "Gabriel", "idade": 20};
-
-//   print(usuario["idade"]);
-//   usuario["idade"] = 25;
-
-//   print(usuario.length);
-// }
-
-void main() {
-  repeticaoDoWhile();
+  dormir() {
+    print("O animal é $tipo");
+  }
 }
 
-repeticaoDoWhile() {
-  int valor = 5;
-
-  do {
-    print("o valor é $valor");
-  } while (true);
+void main() {
+  Animal animal = new Animal();
+  animal.tipo = "cachorro";
 }
