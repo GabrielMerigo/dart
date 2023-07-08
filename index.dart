2101fd0c774b23a7a7a13b8e0a249d6452862d0e
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2023-07-08 14:57:55 -0300

    boolean

--- index.dart
+++ index.dart
@@ -5,10 +5,18 @@
 //   print(name + sobrenome);
 // }
 
-void main() {
-  int valor = 10;
-  double valorQuebrado = 12.2;
+// void main() {
+//   int valor = 10;
+//   double valorQuebrado = 12.2;
 
-  print(valor);
-  print(valorQuebrado);
+//   print(valor);
+//   print(valorQuebrado);
+// }
+
+void main() {
+  bool status = true;
+  bool isTrue = 1 > 0;
+  String resultado = status ? "Verdadeiro" : "Falso";
+
+  print(resultado);
 }
